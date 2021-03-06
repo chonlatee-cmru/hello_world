8a75782738351bc7f29515b896db3b53faa72e42
chonlatee-cmru <chonlatee.intapanya@gmail.com>
2021-03-06 14:44:57 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        drawer: Drawer(
+          child: Text("Hello Drawer"),
+        ),
+        appBar: AppBar(
+          title: Text(
+            "My First Project",
+          ),
+        ),
+        body: Center(
+          child: Text("Hello Chonlatee"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.phone),
+              label: "phone",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard_customize),
+              label: "dashboard",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
